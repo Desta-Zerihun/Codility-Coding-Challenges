cb0851dd0583bbd05554702677e60cab8978da2e
Desta-Zerihun <gammachis2000@gmail.comn>
2023-11-19 08:49:42 +0300

    Finding the missing number

---
+++ src/PermMissingElem.java
@@ -0,0 +1,24 @@
+// This is an implementation for finding the missing element in a given array.
+public class PermMissingElem {
+    public static int findMissing(int arr[]){
+        int actual_sum = 0;
+        int expected_sum = 0;
+        int max_number = arr.length + 1;
+        for(int i:arr){
+            actual_sum += i;
+        }
+        expected_sum = (max_number*(max_number+1)/2);
+        return expected_sum - actual_sum;
+
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = {2,3,1,5};
+        System.out.println(findMissing(arr1));
+        int arr2[] = {1,2,3,4,5,6,7,8,9};
+        System.out.println(findMissing(arr2));
+        int arr3[] = {};
+        System.out.println(findMissing(arr3));
+
+    }
+}
